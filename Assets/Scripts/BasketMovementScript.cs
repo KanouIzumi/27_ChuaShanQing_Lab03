148dc6e3284eebe7f12c746d7b416d51695c5776
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2022-01-13 10:19:05 +0800

    Got score, able to collide with both health and unhealthy stuff
    I was able to put in score function in, I was able to let the unhealth and healthy to let them collide with the basket

--- Assets/Scripts/BasketMovementScript.cs
+++ Assets/Scripts/BasketMovementScript.cs
@@ -1,15 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BasketMovementScript : MonoBehaviour
 {
     public float speed;
-   
+
+    private AudioSource audioSource;
+    public AudioClip[] AudioClipBGMArr;
+
+    private Rigidbody2D rb;
+
+    public float score;
+    public GameObject scoreText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
+        audioSource = GetComponent<AudioSource>();
+        scoreText.GetComponent<Text>().text = "Score: " + score.ToString();
     }
 
     // Update is called once per frame
@@ -25,8 +37,23 @@ public class BasketMovementScript : MonoBehaviour
 
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Healthy"))
+        {
+            score += 10;
+            print("Got Fruit!");
+            Destroy(collision.gameObject);
+            scoreText.GetComponent<Text>().text = "Score: " + score.ToString();
+        }
+
+        else if(collision.gameObject.CompareTag("Unhealthy"))
+        {
+            print("Bad Stuff");
+            Destroy(collision.gameObject);
+        }
+    }
 
 
-    
 
 }
